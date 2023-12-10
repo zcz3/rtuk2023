a3bea2784ff1713982b62f73a977e7aa8b5d326c
James Crickmere <james.crickmere@chamsys.co.uk>
2023-12-10 15:20:14 +0000

    tracktool wip

--- tracks/tracktool.py
+++ tracks/tracktool.py
@@ -3,148 +3,173 @@
 import sys
 import glob
 import xml.etree.ElementTree as ET
-from datetime import datetime
+from datetime import datetime, timezone
 from operator import itemgetter
-
-COMBINED_TRACKS_FILE = 'James_RTUK23_Tracks.gpx'
-
-LEG_FILTER = (
-  ('Gosport to Dover',              ('2023-04-22T15:11:49Z', '2023-04-23T10:47:49Z')),
-  ('Dover to Ramsgate',             ('2023-04-25T14:59:49Z', '2023-04-25T18:44:54Z')),
-  ('Ramsgate to Harwich',           ('2023-04-26T05:50:21Z', '2023-04-26T16:58:53Z')),
-  ('Harwich to Woodbridge',         ('2023-04-27T13:14:04Z', '2023-04-27T17:05:39Z')),
-  ('Woodbridge to Lowestoft',       ('2023-04-30T07:34:46Z', '2023-04-30T16:41:02Z')),
-  ('Lowestoft to Scarborough',      ('2023-05-03T10:39:57Z', '2023-05-04T09:30:06Z')),
-  ('Scarborough to Tyne',           ('2023-05-08T05:50:57Z', '2023-05-08T23:35:11Z')),
-  ('Tyne to Blyth',                 ('2023-05-13T12:55:25Z', '2023-05-13T15:43:06Z')),
-  ('Blyth to Newton Haven ⚓',      ('2023-05-15T09:58:32Z', '2023-05-15T19:34:58Z')),
-  ('Newton Haven ⚓ to Eyemouth',   ('2023-05-16T09:19:34Z', '2023-05-16T19:09:29Z',
-                                     '2023-05-16T20:18:33Z', '2023-05-16T20:47:24Z')),
-  ('Eyemouth to Arbroath',          ('2023-05-18T04:08:11Z', '2023-05-18T12:12:59Z')),
-  ('Arbroath to Peterhead',         ('2023-05-20T12:38:18Z', '2023-05-21T03:26:31Z')),
-  ('Peterhead to Wick',             ('2023-05-25T06:44:48Z', '2023-05-26T05:30:07Z')),
-)
+import csv
 
 
-def combine_trkpts(gpx_list):
-  pts = {} # dict of trkpts indexed by time
+def oh(msg):
+  raise Exception(msg)
 
-  for f in gpx_list:
-    gf = GPXFile()
-    gf.load(f)
+def parse_utc_time_string(ts):
+  d = datetime.strptime(ts, '%Y-%m-%d %H:%M')
+  return d.replace(tzinfo=timezone.utc)
 
-    if not gf.ok:
-      return (False, 'Load error (%s): %s' % (f, gf.error))
-    
-    for p in gf.trkpts:
-      if p['time'] not in pts.keys():
-        pts[p['time']] = p
 
-  return (True, sorted(list(pts.values()), key=itemgetter('time')))
+MODE_LEG_MARKER = 'Stop'
+MODES_STATIC = ('MoorPontoon', 'MoorBuoy', 'Anchor')
+MODES_MOVING = ('Engine', 'Sail', 'MotorSail')
+MODES = (MODE_LEG_MARKER, ) + MODES_STATIC + MODES_MOVING
 
-def filter_points(pts, filter):
-  # pts is list of points (each point being a dict with time, lat, lon, ele in text)
-  # assumes filter legs are in time order and not overlapping
-  # return val is suitable for write_trks()
 
-  n_legs = len(filter)
-  n_pts = len(pts)
-  c_pt = 0
+def read_tracklog(csv_path):
+  tracklog= []
+  last_date = None
+  current_leg = None
 
-  if not n_legs or not n_pts:
-    return (False, 'Empty filter or points')
-  
-  trks = {}
+  with open(csv_path, 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+    for row in reader:
+      date, mode, dest = row
+      date = date.strip()
+      mode = mode.strip()
+      dest = dest.strip()
 
-  for leg_name, leg_segs in filter:
-    rngs = []
-    for i in range(len(leg_segs)):
-      if i & 1:
-        rngs.append((
-          datetime.fromisoformat(leg_segs[i-1]), datetime.fromisoformat(leg_segs[i])
-        ))
-    
-    segs = []
-    
-    for start, end in rngs:
-      seg_pts = []
-  
-      while c_pt < n_pts:
-        pt = pts[c_pt]
-        ptime = datetime.fromisoformat(pt['time'])
+      if len(date) + len(mode) + len(dest) == 0:
+        continue
 
-        if ptime > end:
-          break
-
-        c_pt += 1
-
-        if ptime < start:
-          continue
-
-        seg_pts.append(pt)
+      if mode not in MODES:
+        oh('Invalid mode')
       
-      if len(seg_pts) == 0:
-        return (False, 'Empty segment in ' + leg_name)
+      datep = None
       
-      segs.append(seg_pts)
+      if mode != MODE_LEG_MARKER:
+        datep = parse_utc_time_string(date)
 
-    if len(segs) == 0:
-      return (False, 'Empty leg: ' + leg_name)
+        if last_date is not None and datep <= last_date:
+          oh('Date not sequential')
+        
+        last_date = datep
+
+        if len(dest):
+          oh('Unexpected dest')
+      
+      if mode == MODE_LEG_MARKER:
+        if len(dest) == 0:
+          oh('No dest')
+      
+      if mode == MODE_LEG_MARKER and current_leg:
+        current_leg['stop'] = dest
+        tracklog.append(current_leg)
+        current_leg = None
+
+      if mode == MODE_LEG_MARKER and not current_leg:
+        current_leg = {
+          'start': dest,
+          'stop': '',
+          'tracks': [],
+        }
+
+      if mode != MODE_LEG_MARKER:
+        if not current_leg:
+          oh('Missing leg marker')
+ 
+        current_leg['tracks'].append({
+          'date': datep,
+          'mode': mode,
+          'dest': dest,
+        })
+  
+  if len(current_leg['tracks']):
+    oh('Missing final leg marker')
+
+  return tracklog
+
+
+GPX_NS = 'http://www.topografix.com/GPX/1/1'
+NSP = '{%s}' % GPX_NS
+ET.register_namespace('', GPX_NS)
+
+class GPXWriter:
+
+  def __init__(self):
+    self.root = ET.Element(NSP + 'gpx')
+    self.root.attrib['version'] = '1.1'
+    self.root.attrib['creator'] = 'https://github.com/zcz3/rtuk2023'
+
+    self.cur_trk = None
+    self.cur_trkseg = None
+    self.trkseg_n = 0
+    self.trkpt_n = 0
+  
+  def start_trk(self, name):
+    if self.cur_trk is not None:
+      oh("trk not stopped")
+
+    self.cur_trk = trk = ET.SubElement(self.root, NSP + 'trk')
+    trk_name = ET.SubElement(self.cur_trk, NSP + 'name')
+    trk_name.text = name
+  
+  def stop_trk(self):
+    if self.cur_trk is None:
+      oh("trk not started")
+
+    if self.cur_trkseg is not None:
+      oh("trkseg not stopped")
     
-    trks[leg_name] = segs
+    if self.trkseg_n == 0:
+      oh("no trksegs")
+
+    self.cur_trk = None
+    self.trkseg_n = 0
   
-  if len(trks) == 0:
-    return (False, 'No legs')
+  def start_trkseg(self):
+    if self.cur_trk is None:
+      oh("trk not started")
+
+    if self.cur_trkseg is not None:
+      oh("trkseg not stopped")
+    
+    self.cur_trkseg = ET.SubElement(self.cur_trk, NSP + 'trkseg')
   
-  return (True, trks)
+  def stop_trkseg(self):
+    if self.cur_trk is None:
+      oh("trk not started")
+    
+    if self.cur_trkseg is None:
+      oh("trkseg not started")
+    
+    if self.trkpt_n == 0:
+      oh("no trkpts")
+    
+    self.cur_trkseg = None
+    self.trkpt_n = 0
 
-def write_trks(trks, gpx_name):
-  # trks is dict of track name to list of segments
-  # each segment is a list of points
-  # each point is a dict with time, lat, lon, ele in text
-
-  GPX_NS = 'http://www.topografix.com/GPX/1/1'
-  NSP = '{%s}' % GPX_NS
-  ET.register_namespace('', GPX_NS)
-
-  root = ET.Element(NSP + 'gpx')
-  root.attrib['version'] = '1.1'
-  root.attrib['creator'] = 'https://github.com/zcz3/rtuk2023'
-
-  for tname in trks:
-    tsegs = trks[tname]
-    if len(tsegs) == 0:
-      return False
-
-    trk = ET.SubElement(root, NSP + 'trk')
-    name = ET.SubElement(trk, NSP + 'name')
-    name.text = tname
-
-    for tseg in tsegs:
-      if len(tseg) == 0:
-        return False
-
-      seg = ET.SubElement(trk, NSP + 'trkseg')
-
-      for tpt in tseg:
-        trkpt = ET.SubElement(seg, NSP + 'trkpt')
-        trkpt.attrib['lat'] = tpt['lat']
-        trkpt.attrib['lon'] = tpt['lon']
-        time = ET.SubElement(trkpt, NSP + 'time')
-        time.text = tpt['time']
-        ele = ET.SubElement(trkpt, NSP + 'ele')
-        ele.text = tpt['ele']
+    self.trkseg_n += 1
   
-  tree = ET.ElementTree(root)
-  ET.indent(tree)
+  # All parameters must be strings
+  def add_trkpt(self, time, lat, lon, ele):
+    if self.cur_trkseg is None:
+      oh("trkseg not started")
+    
+    trkpt = ET.SubElement(self.cur_trkseg, NSP + 'trkpt')
+    trkpt.attrib['lat'] = lat
+    trkpt.attrib['lon'] = lon
+    trk_time = ET.SubElement(trkpt, NSP + 'time')
+    trk_time.text = time
+    trk_ele = ET.SubElement(trkpt, NSP + 'ele')
+    trk_ele.text = ele
 
-  tree.write(
-    gpx_name,
-    encoding='unicode',
-    xml_declaration=True)
-  
-  return True
+    self.trkpt_n += 1
 
+  def write_to_file(self, path):
+    tree = ET.ElementTree(self.root)
+    ET.indent(tree)
+
+    tree.write(
+      path,
+      encoding='unicode',
+      xml_declaration=True)
 
 
 
@@ -164,8 +189,8 @@ class GPXFile:
     self.n_trkpts = 0
 
     self.trkpts = [] # list of dicts
-    self.oldest_trkpt = ''
-    self.newest_trkpt = ''
+    self.oldest_trkpt = None
+    self.newest_trkpt = None
 
   def load(self, path):
     self._reset()
@@ -245,8 +270,11 @@ class GPXFile:
     if len(time) == 0 or len(lat) == 0 or len(lon) == 0 or len(ele) == 0:
       return False
     
+    ptime = datetime.fromisoformat(time)
+    
     self.trkpts.append({
       'time': time,
+      'ptime': ptime,
       'lat': lat,
       'lon': lon,
       'ele': ele,
@@ -254,12 +282,35 @@ class GPXFile:
 
     self.n_trkpts += 1
 
-    if len(self.oldest_trkpt) == 0 or time < self.oldest_trkpt:
-      self.oldest_trkpt = time
+    if self.oldest_trkpt is None or ptime < self.oldest_trkpt:
+      self.oldest_trkpt = ptime
     
-    if len(self.newest_trkpt) == 0 or time > self.newest_trkpt:
-      self.newest_trkpt = time
+    if self.newest_trkpt is None or ptime > self.newest_trkpt:
+      self.newest_trkpt = ptime
 
+
+def expand_globs(glob_list):
+  files = []
+  for g in glob_list:
+    files += glob.glob(g)
+
+  return files
+
+def combine_trkpts(files):
+  pts = {} # dict of trkpts indexed by time
+
+  for f in files:
+    gf = GPXFile()
+    gf.load(f)
+
+    if not gf.ok:
+      return (False, 'Load error (%s): %s' % (f, gf.error))
+    
+    for p in gf.trkpts:
+      if p['time'] not in pts.keys():
+        pts[p['time']] = p
+
+  return (True, sorted(list(pts.values()), key=itemgetter('time')))
 
 
 def usage(exec_name):
@@ -271,20 +322,11 @@ Print info about GPS files:
 Combine all tracks from multiple raw GPX files into single track in single GPX file:
   {0} cs combined.gpx raw_garmin73/*.gpx
 
-Combine all tracks and then split again according to LEG_FILTER into single GPX file called \"{1}\":
-  {0} cf raw_garmin73/*.gpx
-
-Combine all tracks and export only those that occur since LEG_FILTER into a single GPX files called \"Recent_Tracks.gpx\":
-  {0} cn newtracks.gpx raw_garmin73/*.gpx
-""".format(exec_name, COMBINED_TRACKS_FILE), file=sys.stderr)
+Combine all tracks and split into legs according to CSV into single GPX file":
+  {0} cf combined.gpx tracklog.csv raw_garmin73/*.gpx
+""".format(exec_name), file=sys.stderr)
   sys.exit(-1)
 
-def expand_globs(glob_list):
-  files = []
-  for g in glob_list:
-    files += glob.glob(g)
-
-  return files
 
 def main(argv):
   ex = argv[0]
@@ -317,8 +359,8 @@ def main(argv):
         print('  Num rte: %i' % gf.n_rte)
         print('  Num trk: %i' % gf.n_trk)
         print('    Num trkpts: %i' % gf.n_trkpts)
-        print('    Oldest trkpt: ' + gf.oldest_trkpt)
-        print('    Newest trkpt: ' + gf.newest_trkpt)
+        print('    Oldest trkpt: ' + gf.oldest_trkpt.isoformat())
+        print('    Newest trkpt: ' + gf.newest_trkpt.isoformat())
         print('\n')
 
   elif cmd == 'cs':
@@ -337,12 +379,17 @@ def main(argv):
       pts = res
       print('Num unique trkpts: %d' % len(pts))
     
-    outpts = {
-      'Combined': [ pts, ]
-    }
+    of = GPXWriter()
+    of.start_trk('Combined')
+    of.start_trkseg()
 
-    if not write_trks(outpts, outpath):
-      print('Could not write output')
+    for p in pts:
+      of.add_trkpt(p['time'], p['lat'], p['lon'], p['ele'])
+    
+    of.stop_trkseg()
+    of.stop_trk()
+
+    of.write_to_file(outpath)
 
   elif cmd == 'cf':
     if nargs < 1:
